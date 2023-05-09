a3151ce44c49a278a904e5da19e74f3509d33044
Armstrong Apps <124223891+armstrongapps@users.noreply.github.com>
2023-05-09 09:55:56 -0400

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CoreUtilities;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace CoreUtilities;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
